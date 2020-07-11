87ba0a7fee76c2c614cdda38d59f36b09fca4e33
Sourav <sourav.kayal@yahoo.com>
2020-07-11 07:36:19 +0530

    Add project files.

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public String GetPerson() 
+        {
+            return "Hello";
+        }
+
+    }
+}
